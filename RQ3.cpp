cb0b96b508f8c7de2ba1f9f3d256dd8deecc272b
MireleSCos <41023784+MireleSCos@users.noreply.github.com>
2019-11-06 15:58:55 -0300

    Questão 3 adicionada

---
+++ RQ3.cpp
@@ -0,0 +1,38 @@
+//Questão 1 do primeirop roteiro - Tiago Maritan
+//Mirele da Silva Costa
+
+#include <iostream>
+#include <cstdlib>
+#include <time.h>
+
+using namespace std;
+
+int main()
+{
+    int i,num_jogadas, valor_dado;
+    srand(time(NULL));
+    while (1){
+
+        float perc_jogadas[6] = {};
+
+        cout << "Digite o numero de jogadas:" << endl;
+        cin >> num_jogadas;
+
+        if (num_jogadas == 0){
+            break;
+        }
+
+        for (i = 0; i < num_jogadas; i++){
+
+            valor_dado = (rand() % 6); // Gera numero randomico de 0 a 6
+            perc_jogadas[valor_dado] = perc_jogadas[valor_dado] + 1;
+
+        }
+        for (i = 0; i < 6; i++){
+            cout << " Percentual que  # "<< i+1 << " surgiu foi " << (perc_jogadas[i] * 100) /num_jogadas << "%"<< endl;
+
+        }
+    }
+    return 0;
+}
+
