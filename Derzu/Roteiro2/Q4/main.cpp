ab47a6267091bd83dc5ab2b2eda1e487ff83f186
Mirele <mirele.si.co@outlook.com>
2020-02-03 19:10:59 -0300

    Questão 4 add

--- Derzu/Roteiro2/Q4/main.cpp
+++ Derzu/Roteiro2/Q4/main.cpp
@@ -4,6 +4,10 @@
 #include "Triangulo.h"
 int main()
 {
-    /* code */
+    Circulo c1 = Circulo("Circulo",3,3.14);
+    Triangulo t1 = Triangulo("Tris",3,2);
+    Quadrado q1 = Quadrado("Quatro", 4,4);
+    
+    cout << " Area do Circulo: " << c1.calcArea() << "\n Area do Quadrado: " << q1.calcArea() << "\n Area do Triangulo:" << t1.calcArea()<< endl;  
     return 0;
 }
